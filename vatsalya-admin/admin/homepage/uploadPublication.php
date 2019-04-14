12202195a6052772f0fe0f23a8828913f6ee6e40
dayanandraut <dayanandraut2015@.com>
2019-04-14 14:14:10 +0530

    Added upload publication

--- vatsalya-admin/admin/homepage/uploadPublication.php
+++ vatsalya-admin/admin/homepage/uploadPublication.php
@@ -1,9 +1,58 @@
-<!-- Creating Form for uploading pic -->
-<div>
-    <form action="" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit" id = "btnFileUpload">
-    </form>
-</div>
-<!-- End of form -->
+<?php
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["upload"])) {
+    $target_dir = "publication/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    // $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    // if($check !== false) {
+    //     echo "File is an image - " . $check["mime"] . ".";
+    //     $uploadOk = 1;
+    // } else {
+    //     echo "File is not an image.";
+    //     $uploadOk = 0;
+    // }
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 5242880) {
+    echo "Sorry, your file is too large. It should be less than 5MB";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" && $imageFileType != "doc" && $imageFileType != "pdf") {
+    echo "Sorry, only JPG, JPEG, PNG, GIF, PDF, DOC files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+}
+?>
+
+<html>
+   <body>
+      
+      <form action="" method="POST" enctype="multipart/form-data">
+         Select File to Upload : <input type="file" name="fileToUpload" id="fileToUpload"/>
+         <input type="submit" value="Upload" name="upload" id="upload"/>
+      </form>
+      
+   </body>
+</html>
