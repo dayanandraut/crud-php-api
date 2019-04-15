f04b05e1acca651947579412e87c7e86af6da4b0
dayanandraut <dayanandraut2015@.com>
2019-04-15 22:28:48 +0530

    Allowed POST in delete methods

--- vatsalya-admin/admin/api/Testimonial/delete.php
+++ vatsalya-admin/admin/api/Testimonial/delete.php
@@ -2,7 +2,7 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: DELETE");
+header("Access-Control-Allow-Methods: DELETE,POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
